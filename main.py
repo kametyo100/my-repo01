a63384c1c85e0008a8509ba00c070c93e10f4175
kametyo100 <parakeet100@gmail.com>
2025-08-31 01:36:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-message='Hello, World!'
+import tkinter as tk
+import tkinter.messagebox as messagebox
+
+message = 'Hello, World!'
 print(message)
+
+root = tk.Tk()
+root.withdraw()  # メインウィンドウを表示しないようにする
+messagebox.showinfo("タイトル", "こんにちは")
